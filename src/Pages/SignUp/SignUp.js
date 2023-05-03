24d5a90d612043d35ff808da40a460411bdf4913
MdTarikulIslam <nayon1820@gmail.com>
2023-05-03 16:11:01 +0600

    register with email and password

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import image from "../../assets/images/login/login.svg";
+import { AuthContext } from "../../contexts/AuthProvider/AuthProvider";
+
 
 const SignUp = () => {
-  const handleSignUp = (e) => {
-    e.preventDefault();
-  };
+    const {createUser} = useContext(AuthContext)
+
+const handleSignUp =(e)=>{
+    e.preventDefault()
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const password = form.password.value;
+
+    console.log(name, email, password);
+
+    createUser(email, password)
+      .then((result) => {
+        const user = result.user;
+        console.log("registered user", user);
+      })
+      .catch((error) => {
+        console.error("register error", error);
+      });
+}
   return (
     <div className="hero w-full my-20">
       <div className="hero-content grid gap-10 grid-cols-2 flex-col lg:flex-row">
@@ -15,7 +34,7 @@ const SignUp = () => {
         <div className="card flex-shrink-0 py-10 w-full max-w-sm shadow-2xl bg-base-100">
           <h1 className="text-5xl text-center font-bold">Register</h1>
 
-          <form onClick={handleSignUp} className="card-body">
+          <form onSubmit={handleSignUp} className="card-body">
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Name</span>
